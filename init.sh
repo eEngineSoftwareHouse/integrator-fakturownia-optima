1e0669f0b317792b1f83c8f11e76f72b80d1ccbc
Paweł Walaszek <pawel.walaszek@eengine.pl>
2025-06-20 00:44:55 +0200

    Update init script and docs

--- init.sh
+++ init.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # Uruchom na początku, aby zbudować kontener (środowisko uruchomieniowe)
+# oraz zainstalować zależności PHP
 
 docker compose build
 docker compose up -d
+docker compose exec php composer install --no-interaction
